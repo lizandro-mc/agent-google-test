b168d48512358d99c1621ab429b830db31ed1e4f
lizandro-mc <lizandro@mctekk.com>
2025-06-05 20:34:29 +0000

    Commit initial

--- instavibe/ally_routes.py
+++ instavibe/ally_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session, Response, stream_with_context
 import json 
 import traceback 
-# REPLACE ME TO ADD IMPORT
+from introvertally import call_agent_for_plan, post_plan_event
 
 
 # It's good practice to use a Blueprint for organizing routes
